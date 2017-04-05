47201f1362a23cd5a3857d91e11fbe76d9ec0002
everysum1 <israeldmatos@gmail.com>
2017-04-04 17:02:39 -0700

    Add CardProduct model & supporting files

---
+++ db/migrate/20170404235842_create_card_products.rb
@@ -0,0 +1,10 @@
+class CreateCardProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :card_products do |t|
+      t.string :name
+      t.date :start_date
+
+      t.timestamps
+    end
+  end
+end
