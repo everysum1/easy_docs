562cf80f68efda1368588f48ca937f6daeecbf28
everysum1 <israeldmatos@gmail.com>
2017-04-04 18:22:09 -0700

    Add base url, headers, & new_user method to Marqeta adapter

--- app/adapters/adapter.rb
+++ app/adapters/adapter.rb
@@ -2,12 +2,21 @@ module Adapter
   require 'rest-client'
 
   class Marqeta
-    @base_url = ENV['BASEURL']
+    @base_url = ENV['BASE_URL']
+    @headers = {
+       :content_type => 'application/json',
+      :accept => 'application/json',
+      :Authorization => "#{ENV['APP_TOKEN']} #{ENV['MASTER_TOKEN']}"
+    }
 
     def url 
       p '*' * 50
       p @base_url
       p '*' * 50
+    end
+
+    def new_user
+      RestClient.post(@base_url, {}, @headers)
     end
   end
 
