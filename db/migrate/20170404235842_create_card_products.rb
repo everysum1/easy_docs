27c46d02c0b011073a84ccdd0cf71f1a0e2d3354
everysum1 <israeldmatos@gmail.com>
2017-04-04 17:54:53 -0700

    Add token fields to migrations

--- db/migrate/20170404235842_create_card_products.rb
+++ db/migrate/20170404235842_create_card_products.rb
@@ -1,6 +1,7 @@
 class CreateCardProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :card_products do |t|
+      t.string :token
       t.string :name
       t.date :start_date
 
