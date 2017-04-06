d7f15ddd1f60476067b7c4c852cc48990df68ff9
everysum1 <israeldmatos@gmail.com>
2017-04-05 17:18:50 -0700

    Delete unnecessary routes & add model resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,10 @@ Rails.application.routes.draw do
   get 'webhooks/new'
 
   resources :users, only: [:new, :create]
+  resources :cards, only: [:new, :create]
+  resources :transactions, only: [:new, :create]
 
-  get 'transactions/new'
-
-  get 'cards/new'
-
-  get 'card_products/new'
-
-  root 'users#new'
-
-  get 'wizard_pages/createUser'
-
-  get 'wizard_pages/createCard'
-
-  get 'wizard_pages/transaction'
-
-  get 'wizard_pages/webhook'
+  root "users#new"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
