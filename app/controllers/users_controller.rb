059f3ba3a68911dc8e5460389cfbbb903f398e39
everysum1 <israeldmatos@gmail.com>
2017-04-06 12:59:14 -0700

    Add funding source to new Card Product creation logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @marqeta = Adapter::MarqetaAdapter.new
+
+    funding = JSON.parse(@marqeta.create_funding)
+
     # CREATE CARD PRODUCT
-    data = JSON.parse(@marqeta.create_card_product)
+    data = JSON.parse(@marqeta.create_card_product(funding['token']))
     session[:card_product] = {
       name: data['name'],
       token: data['token']
     }
+    
+    ap '*' * 50
+    ap 'CARD PRODUCT'
+    ap session[:card_product]
+    ap '*' * 50
     
   end
 
@@ -19,11 +27,12 @@ class UsersController < ApplicationController
       first_name: user['first_name'],
       token: user['token']
     }
+    ap '^' * 50
+    ap 'NEW USER'
+    ap session[:user]
+    ap '^' * 50
     # @user = User.parse_user(data)
-    redirect_to new_card_path(
-      user: session[:user], 
-      card_product: session[:card_product]
-    )
+    redirect_to new_card_path
    
       # REDIRECT & PASS USER/CARDPRODUCT INFO TO NEXT VIEW
 
