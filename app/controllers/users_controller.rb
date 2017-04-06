b86a2f364854a3d7c8de523257bb940458e66847
everysum1 <israeldmatos@gmail.com>
2017-04-05 17:17:19 -0700

    Add new & create actions for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,35 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @marqeta = Adapter::MarqetaAdapter.new
+    # CREATE CARD PRODUCT
+    data = JSON.parse(@marqeta.create_card_product)
+    session[:card_product] = {
+      name: data['name'],
+      token: data['token']
+    }
+    
   end
 
   def create
+    # CREATE USER & SAVE IN DB
+    @marqeta = Adapter::MarqetaAdapter.new
+    user = JSON.parse(@marqeta.create_user)
+    session[:user] = {
+      first_name: user['first_name'],
+      token: user['token']
+    }
+    # @user = User.parse_user(data)
+    redirect_to new_card_path(
+      user: session[:user], 
+      card_product: session[:card_product]
+    )
+   
+      # REDIRECT & PASS USER/CARDPRODUCT INFO TO NEXT VIEW
+
+  
+      # RELOAD PAGE
+    
 
   end
 end
