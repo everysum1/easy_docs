dd7c36cc5a5954b44cb2a5094bc91e8bf6f58c62
everysum1 <israeldmatos@gmail.com>
2017-04-06 12:58:44 -0700

    Add Card create method logic to controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,5 +4,19 @@ class CardsController < ApplicationController
   end
 
   def create
+    # CREATE CARD W/ USER/CARDPRODUCT TOKENS
+    @marqeta = Adapter::MarqetaAdapter.new
+    data = JSON.parse(@marqeta.create_card(
+      session[:user][:token], 
+      session[:card_product][:token]
+    ))
+    session[:card] = data
+    ap '#' * 50
+    ap 'NEW CARD'
+    ap session[:card]
+    ap '#' * 50
+
+    # REDIRECT & PASS CARD INFO TO TRANSACTION VIEW
+    redirect_to new_transaction_path
   end
 end
