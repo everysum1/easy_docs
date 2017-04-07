28950613a8214dedae9b4e0fa179e69ee837c308
everysum1 <israeldmatos@gmail.com>
2017-04-07 13:10:53 -0700

    Add Twilio sms sending logic to Webhooks controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,7 +2,7 @@ class WebhooksController < ApplicationController
   def new
     @webhook = Webhook.new
     @amount = session[:transaction]['request_amount']
-    @user = session[:user]['first_name']
+    @user = session[:user][:first_name]
     @merchant = session[:transaction]['card_acceptor']['name']
     @city = session[:transaction]['card_acceptor']['city']
     @state = session[:transaction]['card_acceptor']['state']
@@ -11,9 +11,15 @@ class WebhooksController < ApplicationController
   end
 
   def create
-    
-    @twilio = Adapter::Twilio.new
-    @twilio.send_message(params[:webhook])
+    @twilio = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+    # @twilio.send_message(params[:webhook])
+    alert_message = "Your Marqeta account has just received an authorization request for #{params['webhook'][:amount]} from #{params['webhook'][:user]} at #{params['webhook'][:merchant]} located in #{params['webhook'][:city]},#{params['webhook'][:state]}, processed over the #{params['webhook'][:network]} network. It is currently in #{params['webhook'][:status]} state."
+
+    @twilio.messages.create(
+      from: ENV['TWILIO_NUMBER'],
+      to:   params['webhook'][:phone],
+      body: alert_message,
+    )
 
     redirect_to webhooks_path
   end
