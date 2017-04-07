d7f409f7d93e5db97fdb77434e227c12f35ad955
everysum1 <israeldmatos@gmail.com>
2017-04-07 00:27:34 -0700

    Add Webhook view variables & initial create action logic

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,7 +1,23 @@
 class WebhooksController < ApplicationController
   def new
+    @webhook = Webhook.new
+    @amount = session[:transaction]['request_amount']
+    @user = session[:user]['first_name']
+    @merchant = session[:transaction]['card_acceptor']['name']
+    @city = session[:transaction]['card_acceptor']['city']
+    @state = session[:transaction]['card_acceptor']['state']
+    @network = session[:transaction]['network']
+    @status = session[:transaction]['state']
   end
 
   def create
+    
+    @twilio = Adapter::Twilio.new
+    @twilio.send_message(params[:webhook])
+
+    redirect_to webhooks_path
+  end
+
+  def index
   end
 end
