52d0725a3b3bf45ee0cf65f8cb60771d8077a04a
everysum1 <israeldmatos@gmail.com>
2017-04-06 23:32:36 -0700

    Add self parsing method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  def self.parse_user(user_object)
+    @user = User.new(
+      token: user_object['token'], 
+      first_name: user_object['first_name']
+    )
+
+    @user
+  end
 end
