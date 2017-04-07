1fa8b6e109be298b751b56cfee4384a5859f22d0
everysum1 <israeldmatos@gmail.com>
2017-04-06 17:27:24 -0700

    Add funding User logic & new Transaction creation in controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,9 +1,27 @@
 class TransactionsController < ApplicationController
   def new
     @transaction = Transaction.new
+    @user = session[:user]
+    funding = session[:funding]
+    @marqeta = Adapter::MarqetaAdapter.new
+    data = JSON.parse(@marqeta.fund_user(@user[:token], funding['token']))
+    ap session[:funding][:amount] = data['amount']
+    ap '@' * 50
+    ap 'NEW USER FUNDED'
+    ap data
+    ap '@' * 50
   end
 
   def create
-    @user = Adapter::Marqeta.new_user
+    # CREATE TRANSACTION W/ CARD TOKEN
+    @marqeta = Adapter::MarqetaAdapter.new
+    card = session[:card]['token']
+    data = JSON.parse(@marqeta.create_transaction(card, params['transaction']['amount']))
+    session[:transaction] = data['transaction']
+    ap '!' * 50
+    ap data
+    ap '!' * 50
+    # REDIRECT TO WEBHOOK PAGE & PASS CARD TOKEN
+    redirect_to new_webhook_path
   end
 end
