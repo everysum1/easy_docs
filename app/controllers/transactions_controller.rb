6050d8a9872ea568fe3886eaac944898c0d12acc
everysum1 <israeldmatos@gmail.com>
2017-04-06 23:27:58 -0700

    Add new transaction creation logic to controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,6 +4,18 @@ class TransactionsController < ApplicationController
   end
 
   def create
-    @user = Adapter::Marqeta.new_user
+    @marqeta = Adapter::MarqetaAdapter.new
+    amount = params['transaction']['amount']
+    card = session[:card]['token']
+
+    transaction = JSON.parse(@marqeta.create_transaction(amount, card))
+    session[:transaction] = transaction
+
+    ap '$' * 50
+    ap 'NEW CARD'
+    ap session[:transaction]
+    ap '$' * 50
+
+    redirect_to new_webhook_path
   end
 end
